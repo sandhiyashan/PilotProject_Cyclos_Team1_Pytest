656737391d6db784cdbc4d70238f82d5107bb118
sandhiyashan <sandhiya110902@gmail.com>
2024-06-05 21:13:12 +0530

    update my advertisement feature

--- Pages/HomePage.py
+++ Pages/HomePage.py
@@ -6,6 +6,8 @@ class HomePage(BasePage):
     register_Xpath = (By.XPATH,"//div[text()=' Register ']")
     login_xpath = (By.XPATH,"(//div[@class='ml-2'])[3]")
     verify_login_xpath = (By.XPATH,"(//div[@class='top-title'])[2]")
+    marketPlace_xpath = (By.XPATH,"//div[text()='Marketplace']")
+    Logout_xpath = (By.XPATH,"//a[@aria-label='Logout']//icon//*[name()='svg']")
 
     def goToRegister(self):
         self.click(self.register_Xpath)
@@ -16,3 +18,9 @@ class HomePage(BasePage):
     def verifyLogin(self):
         title = self.find(self.verify_login_xpath).text
         assert title == 'Cyclos'
+
+    def goToMarketPlace(self):
+        self.click(self.marketPlace_xpath)
+
+    def click_the_logout(self):
+        self.click(self.Logout_xpath)
